c1e40daed3bab9b8d34999a78e8342351163720d
Sergei Mindalev <mindalev1@yandex.ru>
2024-04-10 16:25:01 +0300

    task3_2

--- homework-3/queries.sql
+++ homework-3/queries.sql
@@ -12,10 +12,20 @@ WHERE customers.city=employees.city AND shippers.company_name='United Package'
 -- имя поставщика и его телефон (contact_name и phone в табл suppliers) для таких продуктов,
 -- которые не сняты с продажи (поле discontinued) и которых меньше 25 и которые в категориях Dairy Products и Condiments.
 -- Отсортировать результат по возрастанию количества оставшегося товара.
-
+SELECT product_name, units_in_stock, contact_name, phone FROM products
+	JOIN suppliers USING(supplier_id)
+	JOIN(SELECT category_id, category_name FROM categories
+	WHERE category_name IN ('Dairy Products', 'Condiments')) AS category_filter USING(category_id)
+	WHERE discontinued=0 AND units_in_stock < 25
+	ORDER BY units_in_stock
 
 -- 3. Список компаний заказчиков (company_name из табл customers), не сделавших ни одного заказа
-
+SELECT company_name FROM customers
+WHERE NOT EXISTS(SELECT * FROM orders WHERE orders.customer_id=customers.customer_id)
 
 -- 4. уникальные названия продуктов, которых заказано ровно 10 единиц (количество заказанных единиц см в колонке quantity табл order_details)
 -- Этот запрос написать именно с использованием подзапроса.
+SELECT DISTINCT product_name
+FROM products
+WHERE product_id IN (SELECT product_id FROM order_details WHERE quantity=10)
+ORDER BY product_name
